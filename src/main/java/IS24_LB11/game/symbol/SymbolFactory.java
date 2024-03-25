fae960f62d6e8796ed8b628eb0950cff26fd7037
Zendikcar <gioeleguaiumi@gmail.com>
2024-03-25 17:22:57 +0100

    Updated game's components and utils

--- src/main/java/IS24_LB11/game/symbol/SymbolFactory.java
+++ src/main/java/IS24_LB11/game/symbol/SymbolFactory.java
@@ -6,7 +6,7 @@ public class SymbolFactory {
     public static Symbol fromCharacter(Character c) throws SyntaxException {
         if (Suit.isValidChar(c)) return Suit.fromCharacter(c);
         if (Item.isValidChar(c)) return Item.fromCharacter(c);
-        if (Empty.isValidChar(c)) return new Empty();
+        if (Empty.isValidChar(c)) return Empty.symbol();
         if (c == Symbol.nullChar) return null;
         throw new SyntaxException(String.format(Symbol.INVALID_CHAR_MSG, c));
     }
