ec152301f6f1f2a9ff3ab8532f8d02f55558ac01
johntitor <giovannilauletta@yahoo.it>
2024-04-05 16:56:46 +0200

    Class initialization, fixed json file doc, Deck creation

--- src/main/java/IS24_LB11/game/PlacedCard.java
+++ src/main/java/IS24_LB11/game/PlacedCard.java
@@ -3,4 +3,6 @@ package IS24_LB11.game;
 import IS24_LB11.game.components.PlayableCard;
 import IS24_LB11.game.utils.Position;
 
-public record PlacedCard(PlayableCard card, Position position) { }
+public record PlacedCard(PlayableCard card, Position position) {
+
+}
