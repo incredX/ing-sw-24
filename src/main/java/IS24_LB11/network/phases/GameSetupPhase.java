c38db55c2b3448a2973ed304beeeecdcc937f0bf
incredX <agolli.raul@gmail.com>
2024-04-22 16:47:41 +0200

    Preparations before handling of game phases.

--- src/main/java/IS24_LB11/network/phases/GameSetupPhase.java
+++ src/main/java/IS24_LB11/network/phases/GameSetupPhase.java
@@ -2,14 +2,18 @@ package IS24_LB11.network.phases;
 
 import IS24_LB11.game.Game;
 import IS24_LB11.game.utils.SyntaxException;
+import IS24_LB11.network.ClientHandler;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class GameSetupPhase {
 
-    public static void startPhase(Game game, int players) {
+    public static void startPhase(ClientHandler clientHandler, Game game, int players) {
         try {
             game = new Game(players);
+
+            PickPhase.startPhase(clientHandler, game);
         } catch (SyntaxException e) {
             throw new RuntimeException(e);
         } catch (FileNotFoundException e) {
