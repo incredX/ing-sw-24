d657b588a227f25847d81d7800a24c0e4e534a9c
johntitor <giovannilauletta@yahoo.it>
2024-04-26 17:12:12 +0200

    Improving some tests

--- src/test/java/IS24_LB11/game/components/CardFactoryTest.java
+++ src/test/java/IS24_LB11/game/components/CardFactoryTest.java
@@ -1,14 +1,42 @@
 package IS24_LB11.game.components;
 
 import IS24_LB11.game.utils.SyntaxException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CardFactoryTest {
-    @Test
 
+    @Test
+    @DisplayName( "Testing the correct execution of the methods newPlayableCard and newSerialCard")
+    void testNewCard () throws SyntaxException {
+        assertThrows(SyntaxException.class, () -> CardFactory.newPlayableCard("").asString()); //empty string
+        assertThrows(SyntaxException.class, () -> CardFactory.newPlayableCard("K_EEQFF1QFFA__").asString()); //invalid first character
+        assertThrows(SyntaxException.class, () -> CardFactory.newPlayableCard("P_EEQFF1QFFA__").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newPlayableCard("L_EEQFF1QFFA__").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newPlayableCard("G_EEQjajshFF1QFFA__").asString()); //catching some invalid character inside the string
+        assertThrows(SyntaxException.class, () -> CardFactory.newPlayableCard("Z_EEQFF1QFFA__").asString());
+        assertEquals("G_EEQFF1QFFA__", CardFactory.newPlayableCard("G_EEQFF1QFFA__").asString()); //some valid string
+        assertEquals("NFEF_FF0", CardFactory.newPlayableCard("NFEF_FF0").asString());
+        assertEquals("SEPIE_F0I__FPIA", CardFactory.newPlayableCard("SEPIE_F0I__FPIA").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newSerialCard("").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newSerialCard("G_EEghasuQFF1QFFA__").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newSerialCard("K_EEQFF1QFFA__").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newSerialCard("K_EEQFF1QFFA__").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newSerialCard("G_EEsedrftgyhQFF1QFFA__").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newSerialCard("G_EE}@najQFF1QFFA__").asString());
+        assertThrows(SyntaxException.class, () -> CardFactory.newSerialCard("Z_EEQFF1QFFA__").asString());
+        assertEquals("G_EEQFF1QFFA__", CardFactory.newSerialCard("G_EEQFF1QFFA__").asString());
+        assertEquals("NFEF_FF0", CardFactory.newSerialCard("NFEF_FF0").asString());
+        assertEquals("SEPIE_F0I__FPIA", CardFactory.newSerialCard("SEPIE_F0I__FPIA").asString());
+        assertEquals("O2PPPD0", CardFactory.newSerialCard("O2PPPD0").asString());
+        assertEquals("O3PFFL3", CardFactory.newSerialCard("O3PFFL3").asString());
+    }
+
+    @Test
+    @DisplayName( "Testing the valid PlayableCard creation")
     void testValidPlayableCardCreation () throws SyntaxException {
 
         String [] validId = new String[] {
@@ -25,6 +53,7 @@ public class CardFactoryTest {
     }
 
     @Test
+    @DisplayName( "Testing the invalid PlayableCard creation")
     void testInvalidPlayableCardCreation () throws  SyntaxException {
 
         String [] invalidId = new String [] {
@@ -51,6 +80,7 @@ public class CardFactoryTest {
 
 
     @Test
+    @DisplayName("Same as PlayableCard but using newSerialCard")
     void testValidSerialCardCreation () throws SyntaxException {
         String [] validId = new String []{
                 "O2FFF",
@@ -72,6 +102,7 @@ public class CardFactoryTest {
     }
 
     @Test
+    @DisplayName("Same as PlayableCard but using newSerialCard")
     void testInvalidSerialCardCreation () throws  SyntaxException {
 
         String [] invalidId = new String [] {
