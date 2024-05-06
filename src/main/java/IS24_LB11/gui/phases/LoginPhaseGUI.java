eb9bbbca2dc62922fa4e53efc61b0d25255a1728
GabrieleSinge <gabrielelorenzo.singe@mail.polimi.it>
2024-05-06 16:50:36 +0200

    Initialized GUI controller

---
+++ src/main/java/IS24_LB11/gui/phases/LoginPhaseGUI.java
@@ -0,0 +1,11 @@
+package IS24_LB11.gui.phases;
+
+import IS24_LB11.gui.InputHandlerGUI;
+
+public class LoginPhaseGUI extends ClientGUIState{
+    String username;
+
+    public LoginPhaseGUI(InputHandlerGUI inputHandlerGUI){
+
+    }
+}
