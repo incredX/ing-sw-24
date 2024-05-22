cc490ec78199600c6ecc45e53ed9dd6415b05bdb
incredX <agolli.raul@gmail.com>
2024-05-22 10:57:22 +0200

    smallfix

--- src/main/java/IS24_LB11/gui/scenesControllers/GameSceneController.java
+++ src/main/java/IS24_LB11/gui/scenesControllers/GameSceneController.java
@@ -8,6 +8,7 @@ import IS24_LB11.gui.ImageLoader;
 import IS24_LB11.gui.phases.ClientGUIState;
 import IS24_LB11.gui.phases.GameGUIState;
 import IS24_LB11.gui.scenesControllers.ScoreboardController.AnimationInstruction;
+import IS24_LB11.gui.scenesControllers.ScoreboardController.ScoreboardCoordinates;
 import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -200,37 +201,7 @@ public class GameSceneController extends GenericSceneController{
         setUsernamesBoard();
 
 
-
-        scoreboardPositions.add(new AnimationInstruction(0, 430, 615));
-        scoreboardPositions.add(new AnimationInstruction(1, 508, 615));
-        scoreboardPositions.add(new AnimationInstruction(2, 586, 615));
-        scoreboardPositions.add(new AnimationInstruction(3, 623, 544));
-        scoreboardPositions.add(new AnimationInstruction(4, 545, 544));
-        scoreboardPositions.add(new AnimationInstruction(5, 467, 544));
-        scoreboardPositions.add(new AnimationInstruction(6, 389, 544));
-        scoreboardPositions.add(new AnimationInstruction(7, 389, 473));
-        scoreboardPositions.add(new AnimationInstruction(8, 467, 473));
-        scoreboardPositions.add(new AnimationInstruction(9, 545, 473));
-        scoreboardPositions.add(new AnimationInstruction(10, 623, 473));
-        scoreboardPositions.add(new AnimationInstruction(11, 623, 402));
-        scoreboardPositions.add(new AnimationInstruction(12, 545, 402));
-        scoreboardPositions.add(new AnimationInstruction(13, 467, 402));
-        scoreboardPositions.add(new AnimationInstruction(14, 389, 402));
-        scoreboardPositions.add(new AnimationInstruction(15, 389, 331));
-        scoreboardPositions.add(new AnimationInstruction(16, 467, 331));
-        scoreboardPositions.add(new AnimationInstruction(17, 545, 331));
-        scoreboardPositions.add(new AnimationInstruction(18, 623, 331));
-        scoreboardPositions.add(new AnimationInstruction(19, 623, 260));
-        scoreboardPositions.add(new AnimationInstruction(20, 508, 228));
-        scoreboardPositions.add(new AnimationInstruction(21, 389, 260));
-        scoreboardPositions.add(new AnimationInstruction(22, 389, 189));
-        scoreboardPositions.add(new AnimationInstruction(23, 389, 118));
-        scoreboardPositions.add(new AnimationInstruction(24, 432, 59));
-        scoreboardPositions.add(new AnimationInstruction(25, 505, 45));
-        scoreboardPositions.add(new AnimationInstruction(26, 578, 59));
-        scoreboardPositions.add(new AnimationInstruction(27, 621, 118));
-        scoreboardPositions.add(new AnimationInstruction(28, 621, 189));
-        scoreboardPositions.add(new AnimationInstruction(29, 508, 131));
+        scoreboardPositions = ScoreboardCoordinates.generate();
 
     }
 
