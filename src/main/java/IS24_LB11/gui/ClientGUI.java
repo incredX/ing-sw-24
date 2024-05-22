6925e723122306002dcb7acdf369d84b7e183780
GabrieleSinge <gabrielelorenzo.singe@mail.polimi.it>
2024-05-22 15:07:20 +0200

    Added javadoc to ClientGUI class

--- src/main/java/IS24_LB11/gui/ClientGUI.java
+++ src/main/java/IS24_LB11/gui/ClientGUI.java
@@ -2,17 +2,24 @@ package IS24_LB11.gui;
 
 import IS24_LB11.gui.phases.ClientGUIState;
 import IS24_LB11.gui.phases.LoginGUIState;
-import IS24_LB11.gui.phases.PlayerStateInterface;
 import IS24_LB11.gui.scenesControllers.LoginSceneController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
-
+/**
+ * The ClientGUI class initializes and starts the client-side graphical user interface (GUI) for the application.
+ * It extends the JavaFX Application class and sets up the initial login state.
+ */
 public class ClientGUI extends Application {
 
-    ClientGUIState state;
+    private ClientGUIState state;
 
+    /**
+     * The main entry point for all JavaFX applications. This method is called after the system is ready for the application to begin running.
+     *
+     * @param primaryStage the primary stage for this application, onto which the application scene can be set.
+     * @throws Exception if something goes wrong during initialization.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.state = new LoginGUIState(this);
@@ -20,11 +27,14 @@ public class ClientGUI extends Application {
         LoginSceneController loginSceneController = new LoginSceneController(this.state);
 
         loginSceneController.showStage();
-
     }
 
+    /**
+     * The main method which serves as the entry point for the application.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
-
 }
